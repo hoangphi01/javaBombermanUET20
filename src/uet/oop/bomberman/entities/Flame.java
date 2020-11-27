85773e451476014ad2437f9b6fb32753eeadf01b
tk14shiina <49159641+tk14shiina@users.noreply.github.com>
2020-11-27 23:19:00 +0700

    Add flame

---
+++ src/uet/oop/bomberman/entities/Flame.java
@@ -0,0 +1,18 @@
+package uet.oop.bomberman.entities;
+
+import javafx.scene.image.Image;
+
+public class Flame extends MovingEntity {
+
+    public Flame() {
+
+    }
+    public Flame(int x, int y, Image img) {
+        super(x, y, img);
+    }
+
+    @Override
+    public void update(int x, int y, Image img) {
+
+    }
+}
